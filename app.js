c776edf7b6046957473a007eb0207de4771aa032
Leth4L <carribean_08@mail.ru>
2022-07-27 14:42:57 +0300

    feat: add nodemon, connect to localhost/mestodb

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log(1 + 1);
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello world?');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listen on ${PORT}`);
+});
