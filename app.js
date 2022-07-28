473850716d3f7841da031a680e0506d6ad5569fd
Leth4L <carribean_08@mail.ru>
2022-07-28 12:14:46 +0300

    feat: add routers and controllers for users

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes/user');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.use(express.json());
+
+app.use(routes);
+
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello world?');
 });
 
 app.listen(PORT, () => {
