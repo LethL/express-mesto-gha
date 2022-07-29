c22be8025be1827e898da2d25a00e237fab75667
Leth4L <carribean_08@mail.ru>
2022-07-29 09:46:10 +0300

    feat: add more controllers and routes for users and cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -21,4 +21,30 @@ const deleteCard = (req, res) => {
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
-module.exports = { getCards, createCard, deleteCard };
+const likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((likes) => res.send({ data: likes }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+const dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((likes) => res.send({ data: likes }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports = {
+  getCards,
+  createCard,
+  deleteCard,
+  likeCard,
+  dislikeCard,
+};
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,20 @@
 const router = require('express').Router();
-const { getUsers, getUserById, createUser } = require('../controllers/users');
+const {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUserInfo,
+  updateUserAvatar,
+} = require('../controllers/users');
 
 router.get('/users', getUsers);
 
 router.get('/users/:userId', getUserById);
 
 router.post('/users', createUser);
+
+router.patch('/users/:userId', updateUserInfo);
+
+router.patch('/users/:userId/avatar', updateUserAvatar);
 
 module.exports = router;
