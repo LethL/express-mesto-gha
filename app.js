8176f20becda222df07b0d06a9ebe99d4002bf50
Leth4L <carribean_08@mail.ru>
2022-08-09 09:57:14 +0300

    feat: add celebrate, joi for user and login validation

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { errors } = require('celebrate');
 const router = require('./routes/router');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const { userValidation, loginValidation } = require('./middlewares/validation');
 
 const { PORT = 3000 } = process.env;
 
@@ -11,12 +13,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', loginValidation, login);
+app.post('/signup', userValidation, createUser);
 
 app.use(auth);
 
 app.use(router);
+
+app.use(errors());
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
---
+++ middlewares/validation.js
@@ -0,0 +1,20 @@
+const { celebrate, Joi } = require('celebrate');
+
+const userValidation = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(20),
+    avatar: Joi.string(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+});
+
+const loginValidation = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+});
+
+module.exports = { userValidation, loginValidation };
